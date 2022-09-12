42258d206aa70c72d0781504710fdf13c76db753
Juru Christopher <113340862+Juru-Christopher@users.noreply.github.com>
2022-09-12 13:17:57 +0300

    calculatorApp.cpp

---
+++ calculatorApp.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+void getInput();
+double division();
+double addition();
+double subtraction();
+double multiplication();
+short controlFlow();
+
+int main(){
+  
+  return 0;
+}
+void getInput(){
+  cout<<"Number: ";
+  cin>>num1;
+  
+  cout<<"Opperator: ";
+  cin>>oppr;
+  
+  cout<<"Number: ";
+  cin>>num2;
+  
+  cin.sync();
+  cin.clear();
